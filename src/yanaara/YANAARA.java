adf7c554d48f0e482f40ae9467bccdc6cb351e38
Martijn Liebrand <martijn.liebrand@han.nl>
2017-10-10 11:22:34 +0200

    projectopzet

---
+++ src/yanaara/YANAARA.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yanaara;
+
+import java.awt.*;
+import javax.swing.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+/**
+ *
+ * @author student
+ */
+
+public class YANAARA extends JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        YANAARA frame = new YANAARA();
+        frame.setSize(600, 400);
+        frame.setTitle("YANAARA");
+        frame.createGUI();
+        frame.show();
+    }
+    
+    public void createGUI(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Container window = getContentPane();
+        window.setLayout(new FlowLayout() );
+    }
+    
+}
